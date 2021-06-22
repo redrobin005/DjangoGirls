62eea45deff924e91a6d85f596c48f3d8baea445
Anwin Robin <axr005@student.bham.ac.uk>
2021-06-22 12:30:29 +0100

    bug fix

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
 ]
 # matching URLs coming from mysite.urls will (http://127.0.0.1:8000/) will access the post_list view

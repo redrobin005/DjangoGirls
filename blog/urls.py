a4fdc7f3db43b4e89be7c1e999f7b666d7d26504
Anwin Robin <axr005@student.bham.ac.uk>
2021-06-25 13:10:06 +0100

    Django Form URL, view, template and forms.py(model) added

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # path, view name, url name
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
 # matching URLs coming from mysite.urls will (http://127.0.0.1:8000/) will access the post_list view
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
+from blog.forms import PostForm
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post # dot means current directory
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -9,4 +11,8 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})       
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})    
